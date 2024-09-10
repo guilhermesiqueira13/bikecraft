5b1fe1c35dbbb9905803e2e0cc2ffd786cc00778
guilhermesiqueira13 <guilherme.nunes13@fatec.sp.gov.br>
2024-09-10 13:57:34 -0300

    otimizações do site

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar Items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametros) {
+  const elemento = document.getElementById(parametros);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
